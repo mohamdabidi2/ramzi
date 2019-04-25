2a97b83c552f65e8b833490cf948f1a927572c14
chemagh <44727828+chemagh@users.noreply.github.com>
2019-04-25 15:08:05 +0100

    sec

--- src/Components/Sign/SignUp.jsx
+++ src/Components/Sign/SignUp.jsx
@@ -6,47 +6,103 @@ class SignUp extends Component {
         super(props)
         this.state = {
             firstName:"",
+            ferstnameinput:'',
             lastName:"",
+            lastnameinput:'',
             phone:"",
+            phonenameinput:'',
             adress:"",
+            adressnameinput:'',
             email:"",
+           emailnameinput:'',
             password:"",
+            passwordnameinput:'',
             points:""
 
         }
     }
     firstNameChange = (event) => {
-        if (event.target.value != "") {
-            this.setState({ firstName: event.target.value });
+        if(event.target.value.length<3){
+            this.setState({ ferstnameinput: 'memea' });
+            console.log(event.target.value)
+        }
+        else {
+            this.setState({ firstName: event.target.value,ferstnameinput:'mead' });
             console.log(event.target.value)
         }
     }
     lastNameChange = (event) => {
-        if (event.target.value != "") {
-            this.setState({ lastName: event.target.value });
+     
+        if(event.target.value.length<3){
+            this.setState({ lastnameinput: 'memea' });
+            console.log(event.target.value)
+        }
+        else {
+            this.setState({ lastName: event.target.value,lastnameinput:'mead' });
+            console.log(event.target.value)
         }
     }
     phoneChange = (event) => {
-        if (event.target.value != "") {
-            this.setState({ phone: event.target.value });
+
+          
+        if(event.target.value.length<8){
+            this.setState({phonenameinput: 'memea' });
+            console.log(event.target.value)
+        }
+        else {
+            this.setState({ phone: event.target.value,phonenameinput:'mead' });
+            console.log(event.target.value)
         }
     }
     adressChange = (event) => {
-        if (event.target.value != "") {
-            this.setState({ adress: event.target.value });
+     
+        if(event.target.value.length<4){
+            this.setState({adressnameinput: 'memea' });
+            console.log(event.target.value)
+        }
+        else {
+            this.setState({ adress: event.target.value,adressnameinput:'mead' });
+            console.log(event.target.value)
         }
     }
-    passwordChange = (event) => {
-        if (event.target.value != "") {
-            this.setState({ password: event.target.value });
+    emailChange  = (event) => {
+        if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value)){
+            this.setState({emailnameinput: 'memea' });
+        }
+       
+        else {
+            this.setState({ email: event.target.value ,emailnameinput:'mead'});
         }
     }
-    emailChange = (event) => {
-        if (event.target.value != "") {
-            this.setState({ email: event.target.value });
+    passwordChange= (event) => {
+        let pass=event.target
+        
+            if(pass.value.length<6) {
+                this.setState({passwordnameinput: 'memea' });
+            }
+          
+            if(!/[0-9]/.test(pass.value)) {
+        
+                this.setState({passwordnameinput: 'memea' });
+            }
+           
+            if(!/[a-z]/.test(pass.value)) {
+                this.setState({passwordnameinput: 'memea' });
+            }
+           
+            if(!/[A-Z]/.test(pass.value)) {
+                this.setState({passwordnameinput: 'memea' });
+            }
+        else{
+            this.setState({ password: event.target.value,passwordnameinput:'mead' });
         }
     }
     signup=()=>{
+        if( this.state.ferstnameinput=='memea' || this.state.lastnameinput=='memea' || this.state.phonenameinput=='memea' || this.state.adressnameinput=='memea' || this.state.passwordnameinput=='memea' || this.state.emailnameinput=='memea'  ){
+            alert ('thebt fi les inputs')
+            this.props.history.push('/SignUp')
+        }
+        else{
         axios.post("http://localhost:4000/users", {
       
             firstName:this.state.firstName,
@@ -61,7 +117,7 @@ class SignUp extends Component {
           .catch(err => console.log(err))
         
           this.props.history.push('/SignIn')
-     }
+     }}
    
   
     render() { 
@@ -69,12 +125,12 @@ class SignUp extends Component {
             <section className="section-sign">
                 <h1 className="sign-title">Sing up</h1>
                 <form action="" className="sign-form" >
-                    <input type="text" placeholder="First name" onChange={this.firstNameChange} className="sign-input" />
-                    <input type="text" placeholder="Last name" onChange={this.lastNameChange} className="sign-input" />
-                    <input type="text" placeholder="Phone number" onChange={this.phoneChange} className="sign-input" />
-                    <input type="text" placeholder="Adress" onChange={this.adressChange} className="sign-input" />
-                    <input type="text" placeholder="email" onChange={this.emailChange} className="sign-input" />
-                    <input type="text" placeholder="Password" onChange={this.passwordChange} className="sign-input" />
+                    <input type="text" placeholder="First name" onChange={this.firstNameChange} className='sign-input ' id={this.state.ferstnameinput}  />
+                    <input type="text" placeholder="Last name" onChange={this.lastNameChange} id={this.state.lastnameinput}  className="sign-input" />
+                    <input type="number" placeholder="Phone number" id={this.state.phonenameinput}  onChange={this.phoneChange} className="sign-input" />
+                    <input type="text" placeholder="Adress" id={this.state.adressnameinput}  onChange={this.adressChange} className="sign-input" />
+                    <input type="email" placeholder="email"  id={this.state.emailnameinput}  onChange={this.emailChange} className="sign-input" />
+                    <input type="password" id={this.state.passwordnameinput}  placeholder="Password" onChange={this.passwordChange} className="sign-input" />
                     <div className="sign-redirect">
                         <span className="sign-redirect-text">Already have an account? Click here to </span>
                         <Link className="sign-redirect-link" to="/SignIn">Sign in</Link>
